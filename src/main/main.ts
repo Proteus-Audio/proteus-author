532d0e027e1bad265fe41e29ddcf4246a0da67ef
Adam Howard <adam.howard.music@gmail.com>
2022-08-26 13:08:05 +0100

    Initial Commit + Hello Test

--- src/main/main.ts
+++ src/main/main.ts
@@ -1,30 +1,30 @@
-import {app, BrowserWindow, ipcMain} from 'electron';
-import {join} from 'path';
+import { app, BrowserWindow, ipcMain, dialog } from "electron";
+import { join } from "path";
+import fs from "fs";
 
-function createWindow () {
+function createWindow() {
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      preload: join(__dirname, 'preload.js'),
+      preload: join(__dirname, "preload.js"),
       nodeIntegration: false,
       contextIsolation: true,
-    }
+    },
   });
 
-  if (process.env.NODE_ENV === 'development') {
+  if (process.env.NODE_ENV === "development") {
     const rendererPort = process.argv[2];
     mainWindow.loadURL(`http://localhost:${rendererPort}`);
-  }
-  else {
-    mainWindow.loadFile(join(app.getAppPath(), 'renderer', 'index.html'));
+  } else {
+    mainWindow.loadFile(join(app.getAppPath(), "renderer", "index.html"));
   }
 }
 
 app.whenReady().then(() => {
   createWindow();
 
-  app.on('activate', function () {
+  app.on("activate", function () {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) {
@@ -33,10 +33,19 @@ app.whenReady().then(() => {
   });
 });
 
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
+app.on("window-all-closed", function () {
+  if (process.platform !== "darwin") app.quit();
 });
 
-ipcMain.on('message', (event, message) => {
+ipcMain.on("message", async (event, message) => {
   console.log(message);
-})
+});
+
+ipcMain.handle("openFile", async (event, key) => {
+  const file = await dialog.showOpenDialog({ properties: ["openFile"] });
+  if(file.canceled) return 'canceled';
+  const fileName = (file.filePaths[0].match(/[\w]*\..*$/) || [''])[0];
+  // ipcMain.emit("openFileReturn", { key, filename: "filename" });
+  console.log(fs.readFileSync(file.filePaths[0]))
+  return fileName;
+});
--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,12 +1,28 @@
 <script setup lang="ts">
-import Hello from './components/Hello.vue'
-import { ipcRenderer } from './electron'
+import Hello from "./components/Hello.vue";
+import { ipcRenderer } from "./electron";
+import { ref } from "vue";
 
-ipcRenderer.send('message', 'Hello from App.vue!');
+ipcRenderer.send("message", "Hello from App.vue!");
+const fileName = ref("");
+
+const hello = (): void => {
+  ipcRenderer.send("message", "Hello again!");
+};
+
+const open = async (): Promise<void> => {
+  // console.log(await ipcRenderer.send("openFile", "main"));
+  
+  fileName.value = await ipcRenderer.invoke("openFile", "main");
+
+};
 </script>
 
 <template>
   <div id="app">
-    <Hello/>
+    <Hello />
+    <button @click="hello">Hello!</button><br />
+    {{ fileName }}<br />
+    <button @click="open">Open File</button>
   </div>
 </template>
