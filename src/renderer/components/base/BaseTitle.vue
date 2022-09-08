507a34e0faefb51ad0fd59b3fb1e638d05e63847
Adam Howard <adam.howard.music@gmail.com>
2022-09-08 13:01:43 +0200

    Add Icon to project

---
+++ src/renderer/components/base/BaseTitle.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="title center">
+    <div class="vertical-center">
+      <div class="icon">
+        <img src="/images/icon.png" alt="Proteus" />
+      </div>
+      <h1 class="h1">Proteus.play</h1>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss" scoped>
+.center {
+  text-align: center;
+  height: 8em;
+  display: grid;
+
+  .vertical-center {
+    display: flex;
+    vertical-align: middle;
+    align-content: center;
+    align-items: center;
+    width: fit-content;
+    margin: auto;
+    .h1 {
+      display: inline-block;
+      padding-left: 0.5em;
+    }
+    .icon {
+      display: inline-block;
+      width: 45px;
+
+      img {
+        width: 100%;
+      }
+    }
+  }
+}
+</style>
