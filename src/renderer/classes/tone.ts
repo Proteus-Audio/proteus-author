b8b5b8931218055e8b480b512d8088fcf37d6cc7
Adam Howard <adam.howard.music@gmail.com>
2022-09-21 19:36:59 +0200

    Fix gain moving back to 1 on start/stop

--- src/renderer/classes/tone.ts
+++ src/renderer/classes/tone.ts
@@ -15,6 +15,7 @@ class ToneMaster {
   tracks: ToneTrack[]
   effects: Effect[]
   gain: Gain
+  lastGain: number
   clock: Clock
   cachedCallback: PlayCallback
 
@@ -25,6 +26,7 @@ class ToneMaster {
     this.tracks = []
     this.effects = []
     this.gain = new Gain(1).toDestination()
+    this.lastGain = 1
     this.clock = new Clock()
     this.cachedCallback = () => {}
     this.connectEffects()
@@ -210,13 +212,14 @@ class ToneMaster {
     update()
 
     this.gain.gain.value = 0
-    await this.rampGain(1)
+    await this.rampGain(this.lastGain)
   }
 
   async pause() {
     this.playing = false
     this.clock.pause()
 
+    this.lastGain = this.gain.gain.value
     await this.rampGain(0)
     this._executeOnAllPLayers('stop')
   }
@@ -226,6 +229,7 @@ class ToneMaster {
     this.clock.stop()
     this.cachedCallback = () => {}
 
+    this.lastGain = this.gain.gain.value
     await this.rampGain(0)
   }
 }
