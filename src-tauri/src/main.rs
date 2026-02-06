dbb7ddce73f0cd1b5a249f300aebd96a6b5b5e0d
Adam Howard <adam.thomas.howard@gmail.com>
2026-02-06 14:21:42 +0100

    Update tauri version

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -17,15 +17,17 @@ use file::*;
 use player::*;
 use project::*;
 use proteus_lib::playback::player::Player;
+use dotenv::dotenv;
 
 fn main() {
+    dotenv().ok();
     let app = tauri::Builder::default()
         .plugin(tauri_plugin_global_shortcut::Builder::new().build())
         .plugin(tauri_plugin_os::init())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_clipboard_manager::init())
-        .plugin(tauri_plugin_http::init())
+        // .plugin(tauri_plugin_http::init())
         .plugin(tauri_plugin_notification::init())
         .plugin(tauri_plugin_process::init())
         .plugin(tauri_plugin_fs::init())
