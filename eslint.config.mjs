ac6078999beba04049f7539449519b338a7142cc
Adam Howard <adam.thomas.howard@gmail.com>
2026-02-06 16:35:21 +0100

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,39 +1,9 @@
-import { dirname } from "node:path";
-import { fileURLToPath } from "node:url";
-import { FlatCompat } from "@eslint/eslintrc";
-import js from "@eslint/js";
-import { defineConfig, globalIgnores } from "eslint/config";
-import globals from "globals";
+import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
+import pluginVue from 'eslint-plugin-vue'
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
-
-const compat = new FlatCompat({
-	baseDirectory: __dirname,
-	recommendedConfig: js.configs.recommended,
-	allConfig: js.configs.all,
-});
-
-export default defineConfig([
-	{
-		languageOptions: {
-			globals: {
-				...globals.node,
-			},
-			ecmaVersion: "ESNext",
-			parserOptions: {},
-		},
-		extends: compat.extends(
-			"plugin:vue/vue3-essential",
-			"eslint:recommended",
-			"@vue/typescript/recommended",
-			"plugin:prettier/recommended",
-		),
-		rules: {
-			"no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-			"no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-			"@typescript-eslint/no-var-requires": "off",
-			"@typescript-eslint/no-empty-function": "off",
-		},
-	},
-	globalIgnores(["src/components/playground/", "src/vite-env.d.ts"]),
-]);
+export default defineConfigWithVueTs(
+  eslintPluginPrettierRecommended,
+  pluginVue.configs['flat/essential'],
+  vueTsConfigs.recommendedTypeChecked,
+)
